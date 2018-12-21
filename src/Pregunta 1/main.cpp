5c38365a237f8a66c859c30abbcf7302c18e62c0
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2018-12-20 22:32:27 -0300

    cree la clase piedra

--- src/Pregunta 1/main.cpp
+++ src/Pregunta 1/main.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+class Piedra
+{
+  private:
+    int tipo;
+    bool hunde;
+    int distancia;
+
+  public:
+    Piedra(int tipo, int distancia);
+    int getDistancia();
+    bool estaHundida();
+    void seHundio();
+}
+
+Piedra::Piedra(int tipo, int distancia)
+{
+    this->tipo = tipo;
+    this->distancia = distancia;
+    this->hunde = false;
+}
+
+int Piedra::getDistancia()
+{
+    return this->distancia;
+}
+
+bool Piedra::estaHundida()
+{
+    return this->hunde;
+}
+
+void Piedra::seHundio()
+{
+    this->hunde = true;
+}
+
+main(int argc, char const *argv[])
 {
     cout << "Hola Mundo" << endl;
 
