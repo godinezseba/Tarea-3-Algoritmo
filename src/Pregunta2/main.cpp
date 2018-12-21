ae2e6eb11ad403101b3e584c077a2f728c10b0d4
Daniel Toro Vega <daniel.torov@sansano.usm.cl>
2018-12-21 20:22:43 -0300

    funcion helper

--- src/Pregunta2/main.cpp
+++ src/Pregunta2/main.cpp
@@ -1,16 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <iostream>
 
 
 using namespace std;
+
+// Encuentra la subsecuencia creciente mas grande 
+int helper(int *array,int var){
+
+    int i;
+    int j;
+    int max;
+    int maxGlobal;
+
+    maxGlobal = 0;
+
+    int *data = new int[var];
+    data[0] = 1;
+
+    for (i = 1; i<var; i++){
+        max = 0;
+        for(j =0;j<i;j++){
+            if(array[i]>array[j] && data[j] > max){
+                max = data[j];
+            }
+        }
+        
+        data [i] = max + 1;
+        if (data[i] > maxGlobal) maxGlobal =data[i];
+    }
+
+
+    delete [] data;
+    return maxGlobal;
+}
 
 int main(int argc, char const *argv[])
 {
     int s;
     int i;
     int var;
+    int resp;
 
     while(cin >> s){
         int *data = new int[s];
@@ -20,6 +50,10 @@ int main(int argc, char const *argv[])
 
 
         }
+
+        resp = helper(data,s);
+
+        cout << s - resp << endl;
         delete [] data;
 
     }
