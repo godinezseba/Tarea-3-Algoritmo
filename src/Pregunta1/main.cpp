eb60d4c5204e0a0b4f5b0eb34bda6ef526842a3f
Sebastian Godinez <sebastian.godinez@hotmail.com>
2018-12-21 00:48:40 -0300

    termine la 1, con el planteamiento del Dani, ver si funcinoa siempre xd

--- src/Pregunta1/main.cpp
+++ src/Pregunta1/main.cpp
@@ -51,71 +51,72 @@ void Piedra::seHundio(){
     this->hunde = true;
 }
 
+int mayor(int distancia, Piedra A, Piedra B){
+    int distPiedras;
+    distPiedras = abs(A.getDistancia() - B.getDistancia());
+    if(distancia > distPiedras){
+        return distancia;
+    }
+    else{
+        return distPiedras;
+    }
+}
+
 int main(int argc, char const *argv[]){
     int cantidad, distanciatotal;
     string cadena;
     string temptipo;
     int tempdist;
-    Piedra camino[cantidad];
+    Piedra camino[cantidad + 2];
 
     // solicito datos:
     cin >> cantidad >> distanciatotal;
-    for(int i = 0; i < cantidad; i++){
+    for(int i = 1; i <= cantidad; i++){
         cin >> cadena;
         temptipo = cadena.at(0);
         tempdist = stoi(cadena.substr(2), nullptr);
         camino[i] = Piedra(temptipo, tempdist);
     }
-
-    cout << cantidad << " " << distanciatotal << endl;
-    
-    // for(int i = 0; i < cantidad; i++){
+    // agrego los extremos
+    camino[0] = Piedra("I", 0);
+    camino[cantidad + 1] = Piedra("D", distanciatotal);
+    // for(int i = 0; i < cantidad+2; i++){
     //     cout << camino[i].getDistancia() << " " << camino[i].getTipo() << endl;
     // }
     
-    int recorrido = 0;
+    int distancia = abs(camino[1].getDistancia() - camino[0].getDistancia());
     // CALCULO DE IDA:
     int i = 0; // posicion piedra actual
-    while( i + 2 < cantidad ){ // mientras pueda revisar 2 piedras mas adelante:
+    while( i + 1 < cantidad + 2 ){ // mientras no este en la ultima piedra
+        if(camino[i + 1].getTipo() == "D"){ // si me quedan 2 piedras y entremedio tengo una chica, termino
+            distancia = mayor(distancia, camino[i], camino[i + 1]);
+            i = i + 1;
+        }
         // planteamos la condicion del Dani:
-        if(camino[i+1].getTipo() == "P" && camino[i+2].getTipo() == "P"){
-            camino[i+2].seHundio(); // me muevo 2 posiciones
+        else if(camino[i + 1].getTipo() == "P" && camino[i + 2].getTipo() == "P"){
+            camino[i + 2].seHundio(); // me muevo 2 posiciones
+            distancia = mayor(distancia, camino[i], camino[i+2]);
             i = i + 2;
         } else{ // sino me muevo 1
-            if(camino[i+1].getTipo() == "P"){
-                camino[i+1].seHundio();
+            if(camino[i + 1].getTipo() == "P"){
+                camino[i + 1].seHundio();
             }
+            distancia = mayor(distancia, camino[i], camino[i+1]);
             i++;
-        }
-        recorrido ++;
-    }
-    // ultimas piedras sin analizar
-    if(cantidad - i == 3){ // me quedan 2 piedras
-        if(camino[i+1].getTipo() == "P" && camino[i+2].getTipo() == "P"){
-            camino[i+2].seHundio(); // me muevo 2 posiciones
-            i = i + 2;
-            recorrido ++;
-        } else{ // sino me muevo 1
-            if(camino[i+1].getTipo() == "P"){
-                camino[i+1].seHundio();
-            }
-            i++;
-        }
-    }else{
-        if(camino[i+1].getTipo() == "P"){
-            camino[i+1].seHundio();
         }
     }
     // CALCULO DE VUELTA:
-    
-    for(int i = cantidad-1; i >= 0 ; i++){
+    i = cantidad + 1; // analizo desde el extremo derecho
+    int otro; // posicion de la otra piedra
+    while(i-1 >= 0){ // mientras este en una piedra y no en el extremo izquierdo
         // si la piedra no esta hundida, la recorro
-        if (not(camino[i].estaHundida())) {
-            recorrido ++;
-        }
+        otro = i-1;
+        while(otro - 1 >= 0 && camino[otro].estaHundida()) otro --; // busco hasta obtener una piedra que no este hundida
+        distancia = mayor(distancia, camino[i], camino[otro]);
+        i = otro;
     }
     // imprimo el resultado
-    cout << recorrido << endl;
+    cout << distancia << endl;
 
     return 0;
 }
