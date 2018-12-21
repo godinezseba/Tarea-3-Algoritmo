6f69a46733a608504e551dfedccb984c0040e648
Daniel Toro Vega <daniel.torov@sansano.usm.cl>
2018-12-21 19:55:09 -0300

    Main

--- src/Pregunta2/main.cpp
+++ src/Pregunta2/main.cpp
@@ -1,10 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <iostream>
+
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Hola Mundo" << endl;
+    int s;
+    int i;
+    int var;
+
+    while(cin >> s){
+        int *data = new int[s];
+        for(i=0;i<s;i++){
+            cin >> var;
+            data[i] = var;
+
+
+        }
+        delete [] data;
+
+    }
 
     return 0;
 }
